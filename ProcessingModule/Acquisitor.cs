1318790d7a8e3fdc5b0f3676bf767cd9b31239b8
danijela76 <danijelaleladanijela@gmail.com>
2026-04-26 09:21:30 +0200

    feat: implement Acquisitor periodic polling (digital every 2s, analog every 3s)

--- ProcessingModule/Acquisitor.cs
+++ ProcessingModule/Acquisitor.cs
@@ -8,12 +8,12 @@ namespace ProcessingModule
     /// Class containing logic for periodic polling.
     /// </summary>
     public class Acquisitor : IDisposable
-	{
-		private AutoResetEvent acquisitionTrigger;
+    {
+        private AutoResetEvent acquisitionTrigger;
         private IProcessingManager processingManager;
         private Thread acquisitionWorker;
-		private IStateUpdater stateUpdater;
-		private IConfiguration configuration;
+        private IStateUpdater stateUpdater;
+        private IConfiguration configuration;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Acquisitor"/> class.
@@ -23,48 +23,75 @@ namespace ProcessingModule
         /// <param name="stateUpdater">The state updater.</param>
         /// <param name="configuration">The configuration.</param>
 		public Acquisitor(AutoResetEvent acquisitionTrigger, IProcessingManager processingManager, IStateUpdater stateUpdater, IConfiguration configuration)
-		{
-			this.stateUpdater = stateUpdater;
-			this.acquisitionTrigger = acquisitionTrigger;
-			this.processingManager = processingManager;
-			this.configuration = configuration;
-			this.InitializeAcquisitionThread();
-			this.StartAcquisitionThread();
-		}
+        {
+            this.stateUpdater = stateUpdater;
+            this.acquisitionTrigger = acquisitionTrigger;
+            this.processingManager = processingManager;
+            this.configuration = configuration;
+            this.InitializeAcquisitionThread();
+            this.StartAcquisitionThread();
+        }
 
-		#region Private Methods
+        #region Private Methods
 
         /// <summary>
         /// Initializes the acquisition thread.
         /// </summary>
-		private void InitializeAcquisitionThread()
-		{
-			this.acquisitionWorker = new Thread(Acquisition_DoWork);
-			this.acquisitionWorker.Name = "Acquisition thread";
-		}
+        private void InitializeAcquisitionThread()
+        {
+            this.acquisitionWorker = new Thread(Acquisition_DoWork);
+            this.acquisitionWorker.Name = "Acquisition thread";
+        }
 
         /// <summary>
         /// Starts the acquisition thread.
         /// </summary>
 		private void StartAcquisitionThread()
-		{
-			acquisitionWorker.Start();
-		}
+        {
+            acquisitionWorker.Start();
+        }
 
         /// <summary>
-        /// Acquisitor thread logic.
+        /// Acquisitor thread logic - fires every second, reads config items based on their acquisition interval.
         /// </summary>
 		private void Acquisition_DoWork()
-		{
-            //TO DO: IMPLEMENT
+        {
+            while (true)
+            {
+                // Timer thread signals this every 1 second
+                acquisitionTrigger.WaitOne();
+
+                foreach (IConfigItem configItem in configuration.GetConfigurationItems())
+                {
+                    configItem.SecondsPassedSinceLastPoll++;
+
+                    if (configItem.SecondsPassedSinceLastPoll >= configItem.AcquisitionInterval)
+                    {
+                        configItem.SecondsPassedSinceLastPoll = 0;
+                        try
+                        {
+                            processingManager.ExecuteReadCommand(
+                                configItem,
+                                configuration.GetTransactionId(),
+                                configuration.UnitAddress,
+                                configItem.StartAddress,
+                                configItem.NumberOfRegisters);
+                        }
+                        catch (Exception ex)
+                        {
+                            stateUpdater.LogMessage($"{ex.TargetSite.ReflectedType.Name}.{ex.TargetSite.Name}: {ex.Message}");
+                        }
+                    }
+                }
+            }
         }
 
         #endregion Private Methods
 
         /// <inheritdoc />
         public void Dispose()
-		{
-			acquisitionWorker.Abort();
+        {
+            acquisitionWorker.Abort();
         }
-	}
+    }
 }
